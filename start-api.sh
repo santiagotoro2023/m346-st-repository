eaaab6aa0a99858d4b629dd0dc42a444b4fb0213
santiagotoro2023 <santiago.toro@sidmar.ch>
2025-06-25 11:55:43 +0000

    fix: Pathing for GitHub fixed

--- start-api.sh
+++ start-api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/admin/venv/bin/activate
-python3 /home/admin/flask-api.py
+source /home/admin/m346-st-repository/venv/bin/activate
+python3 /home/admin/m346-st-repository/flask-api.py
